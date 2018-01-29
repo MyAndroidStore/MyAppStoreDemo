f1afe7802b56ccf476a8824e6e4df5d936a4ee9f
Pikachu <55527267@qq.com>
2018-01-29 17:08:48 +0800

    2018-01-29

--- app/src/main/java/org/macpro/appstore/base/BaseActivity.java
+++ app/src/main/java/org/macpro/appstore/base/BaseActivity.java
@@ -4,12 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.RelativeLayout;
 
